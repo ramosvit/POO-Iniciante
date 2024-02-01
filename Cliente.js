669b2ac4a67e482594c12d218e0ec0d714041c6a
Vitor Araujo Ramos <vitor.ramos.ara@gmail.com>
2024-02-01 20:25:11 -0300

    POO-Iniciante

---
+++ Cliente.js
@@ -0,0 +1,13 @@
+export class Cliente{
+    nome;
+    _cpf;
+
+    get cpf(){
+        return this._cpf;
+    }
+
+    constructor(nome, cpf){
+        this.name = nome;
+        this._cpf = cpf;
+    }
+}
